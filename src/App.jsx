8c156c2420e1a813791361e460417a2803346c3f
Ranz15 <randi.harmos@gmail.com>
2023-10-01 20:44:20 +0700

    test atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import InputForm from "./components/fragments/InputForm";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold text-red-400 underline">Hello</h1>
+        <form action="#">
+          <div className="flex flex-col min-h-screen justify-center items-center gap-2">
+            <h1>Selamat datang</h1>
+            <InputForm />
+          </div>
+        </form>
       </div>
     </>
   );
---
+++ src/components/elements/input/input.jsx
@@ -0,0 +1,13 @@
+const Input = () => {
+  <div className="flex flex-col">
+    <label htmlFor="username">Username</label>
+    <input
+      type="text"
+      name="username"
+      id="username"
+      placeholder="Isikan username"
+    />
+  </div>;
+};
+
+export default Input;

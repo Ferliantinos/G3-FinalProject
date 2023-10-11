836bebca86d688fd8de3631d24f5c83c3273cb2e
agungardneham <agungmahendra1305@gmail.com>
2023-10-11 21:52:53 +0700

    added header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/loginPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/layouts/header";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default LoginPage;

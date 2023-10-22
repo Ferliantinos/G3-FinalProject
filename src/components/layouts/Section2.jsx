72b805b48dbd568b66662411c40b215186c252a1
Anis Fatoni <anfa2597@gmail.com>
2023-10-22 21:50:57 +0700

    Added section 4-6

--- src/components/layouts/Section2.jsx
+++ src/components/layouts/Section2.jsx
@@ -1,6 +1,9 @@
 import Content_1 from "../fragments/Content_1";
 import Content_2 from "../fragments/Content_2";
 import Content_3 from "../fragments/Content_3";
+import Content_4 from "../fragments/Content_4";
+import Content_5 from "../fragments/Content_5";
+import Content_6 from "../fragments/Content_6";
 
 const Section2 = () => {
   return (
@@ -9,6 +12,9 @@ const Section2 = () => {
         <Content_1 />
         <Content_2 />
         <Content_3 />
+        <Content_4 />
+        <Content_5 />
+        <Content_6 />
       </section>
     </>
   );
